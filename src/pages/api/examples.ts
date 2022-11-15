80d825ea70797a1f94e781548e3b7bfe7ad9e5dd
Liz Ainslie <lizainslie16@gmail.com>
2022-11-15 06:13:36 -0700

    chore(api): remove example boiler

--- src/pages/api/examples.ts
+++ src/pages/api/examples.ts
@@ -3,8 +3,8 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../server/db/client";
 
 const examples = async (req: NextApiRequest, res: NextApiResponse) => {
-  const examples = await prisma.example.findMany();
-  res.status(200).json(examples);
+  // const examples = await prisma.example.findMany();
+  res.status(200).json({});
 };
 
 export default examples;
