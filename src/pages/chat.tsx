11477f6476b3095a12073345f722bbb0aafb1480
Liz Ainslie <lizainslie16@gmail.com>
2022-11-15 06:15:42 -0700

    fix(chat): remove bad bracket

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import { WSProvider, useWS } from "../utils/ws";}
+import { WSProvider, useWS } from "../utils/ws";
 
 const ChatPage: NextPage = () => {
 	return (
