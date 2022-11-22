1c1b2cc79a278d96133efaedacacd7615fda47c7
Liz Ainslie <lizainslie16@gmail.com>
2022-11-22 08:59:53 -0700

    feat(ui): refine message design

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -9,10 +9,17 @@ type MessageProps = {
 
 const Message: FC<MessageProps> = ({ message }) => {
     return (
-        <div className="py-4 transform-[translateY(-5rem)]" id={`message_${message.id}`}>
-            <h1 className='font-bold'>{message.author.name}</h1>
-            <p>{message.content}</p>
-            <p>{new Date(message.createdTimestamp).toLocaleString()}</p>
+        <div className="py-2 px-6" id={`message_${message.id}`}>
+            <div className='flex gap-x-3'>
+                <img className='inline-block h-12 w-12 rounded-full ring-2 ring-white dark:ring-gray-800' src={message.author.image ?? '/placeholder_avatar.svg'} alt={`${message.author.name}'s Avatar`} />
+                <div className='flex flex-col'>
+                    <div className='flex items-end gap-x-3'>
+                        <span className='font-bold'>{message.author.name}</span>
+                        <span className='text-slate-700 dark:text-slate-400 text-sm'>{new Date(message.createdTimestamp).toLocaleString()}</span>
+                    </div>
+                    <p>{message.content}</p>
+                </div>
+            </div>
         </div>
     );
 };
